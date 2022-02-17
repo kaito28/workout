11c6253d7e021e886cd62874261bda055ec1ed1e
kaito28 <kaito8317@gmail.com>
2022-02-17 13:00:43 +0000

    [update] editアクション追加

--- app/controllers/post_gyms_controller.rb
+++ app/controllers/post_gyms_controller.rb
@@ -1,12 +1,12 @@
 class PostGymsController < ApplicationController
 
   def new
-  @post_gyms = PostGym.new
+    @post_gym = PostGym.new
   end
 
   def create
   @post_gym = PostGym.new(post_gym_params)
-  @post_gym .user_id = current_user.id
+  @post_gym.user_id = current_user.id
   @post_gym.save
   redirect_to post_gyms_path
 
@@ -17,10 +17,26 @@ class PostGymsController < ApplicationController
   end
 
   def show
+  @post_gym = PostGym.find(params[:id])
+  end
+
+  def edit
+  @post_gym = PostGym.find(params[:id])
+  end
+
+  def update
+  post_gym = PostGym.find(params[:id])
+  post_gym.update(post_gym_params)
+  redirect_to post_gyms_path(post_gym)
   end
 
   def destroy
+  @post_gym = PostGym.find(params[:id])
+  @post_gym.destroy
+  redirect_to post_gyms_path
   end
+
+
 
   private
 
