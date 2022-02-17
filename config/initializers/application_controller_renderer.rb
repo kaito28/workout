b19719aca8ed4164dfde89eb94d94e738891eec8
kaito28 <kaito8317@gmail.com>
2022-02-17 13:00:43 +0000

    [update] editアクション追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '665cb25b3d3e9f8265bf72e2d445d7e2b8e3d799e238212e91e0752b3c1bd2378289191f1200278acc960ad59c303a609bfa0f31d2138d51fbb0540efdcdd702'
