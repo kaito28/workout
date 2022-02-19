353cef5ed93dcb29ac16cfa84ef5ecef883dfb4d
kaito28 <kaito8317@gmail.com>
2022-02-19 11:41:13 +0000

    [Add]　マイページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
    root to: 'homes#top'
-  resources :post_gyms, only: [:new, :create, :index, :show, :destroy, :edit] do
+  resources :post_gyms  do
   resource :likes, only: [:create, :destroy]
   resources :comments, only: [:create, :destroy]
   end
+
+  resources :users, only: [:show, :edit, :update]
+
 end
